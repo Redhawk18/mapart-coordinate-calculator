da9fa2ec2193f3c3e56d601f8a8e987ece20b412
Redhawk18 <crange76@gmail.com>
2024-03-30 02:52:33 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# mapart-coordinate-caluclater
+# mapart-coordinate-calculator
 Takes a origin and sizes and returns the corners of the mapart
+
+## Usage
+1. `python3 main.py`
+2. you'll see a prompt like this and fill it out like so 
+```
+Enter the size for example: 6 4
+Enter the Northeast coordinate of the mapart below the noobline. 0 -60 0
+piece: 0 0
+0 127
+-60 -59
+0 127
+
+piece: 0 1
+0 127
+-60 -59
+128 255
+
+piece: 0 2
+0 127
+-60 -59
+256 383
+
+piece: 0 3
+0 127
+-60 -59
+384 511
+
+piece: 1 0
+128 255
+-60 -59
+0 127
+...
+```
---
+++ main.py
@@ -0,0 +1,48 @@
+MAPART_SIZE = 128
+
+
+class Coordinate:
+
+    def __init__(self, x: int, y: int, z: int) -> None:
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+x, z = input("Enter the size for example: ").split()
+row = int(x)
+colunm = int(z)
+
+x, y, z = input(
+    "Enter the Northeast coordinate of the mapart below the noobline. "
+).split()
+orgin = Coordinate(int(x), int(y), int(z))
+
+northeast = Coordinate(orgin.x, orgin.y, orgin.z)
+southwest = Coordinate(
+    orgin.x + MAPART_SIZE - 1, orgin.y + 1, orgin.z + MAPART_SIZE - 1
+)
+
+for i in range(row):
+    northeast.z = orgin.z
+    southwest.z = orgin.z + MAPART_SIZE - 1
+
+    for j in range(colunm):
+        print("piece:", i, j)
+        if j == 0:  # we already did the work above
+            print(northeast.x, southwest.x)
+            print(northeast.y, southwest.y)
+            print(northeast.z, southwest.z)
+            print()
+            continue
+
+        northeast.z += MAPART_SIZE
+        southwest.z += MAPART_SIZE
+
+        print(northeast.x, southwest.x)
+        print(northeast.y, southwest.y)
+        print(northeast.z, southwest.z)
+        print()
+
+    northeast.x += MAPART_SIZE
+    southwest.x += MAPART_SIZE
